7b9e52bd5022902b17ac1b00d8b878769a397183
Joseph Ruiz <ruizj3@uw.edu>
2020-02-14 01:17:41 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,6 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
-      #primary key of :id is created for us!
       t.string :name
       t.string :day
       t.string :network

0762d5f356d5379d159ec62f417c6f68572ac069
Joseph Ruiz <ruizj3@uw.edu>
2020-02-14 01:18:21 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,8 +2,8 @@ class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
       t.string :name
-      t.string :day
       t.string :network
+      t.string :day
       t.integer :rating
     end
   end
